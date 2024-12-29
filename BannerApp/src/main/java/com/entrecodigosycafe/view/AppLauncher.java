78a96cb432671b5667cb8259818e83b12ed73888
Jesús Canales <pjecanal@cibertec.edu.pe>
2024-12-29 16:11:37 -0500

    App Banner de Entre Códigos y Café

---
+++ BannerApp/src/main/java/com/entrecodigosycafe/view/AppLauncher.java
@@ -0,0 +1,7 @@
+package com.entrecodigosycafe.view;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        System.out.println("Aplicación Banner de Entre Códigos y Café");
+    }
+}
