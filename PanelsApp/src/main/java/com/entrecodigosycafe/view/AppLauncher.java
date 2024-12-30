53fd7f3c4f872f5ec8fd4189c38cb5c019beb12e
Jesús Canales <pjecanal@cibertec.edu.pe>
2024-12-29 23:24:03 -0500

    Hemos creado la ventana

--- PanelsApp/src/main/java/com/entrecodigosycafe/view/AppLauncher.java
+++ PanelsApp/src/main/java/com/entrecodigosycafe/view/AppLauncher.java
@@ -2,6 +2,6 @@ package com.entrecodigosycafe.view;
 
 public class AppLauncher {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo, soy el app de paneles");
+        new PanelForm();
     }
 }
---
+++ PanelsApp/src/main/java/com/entrecodigosycafe/view/PanelForm.java
@@ -0,0 +1,13 @@
+package com.entrecodigosycafe.view;
+
+import javax.swing.*;
+
+public class PanelForm extends JFrame {
+    public PanelForm () {
+        this.setTitle("App de Paneles");
+        this.setSize(600, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
