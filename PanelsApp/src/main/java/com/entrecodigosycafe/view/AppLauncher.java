4e3968c5d294e5328c476fe3201075a486ed8f4e
Jesús Canales <pjecanal@cibertec.edu.pe>
2024-12-29 23:12:40 -0500

    Creando App Paneles

---
+++ PanelsApp/src/main/java/com/entrecodigosycafe/view/AppLauncher.java
@@ -0,0 +1,7 @@
+package com.entrecodigosycafe.view;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo, soy el app de paneles");
+    }
+}
