4a8962d4168514094d3b0bb1459b6493700b5c3c
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-10 17:56:18 -0500

    Hemos agregado los 3 paneles de la bandera de la bandera peruana

--- flagPeru/src/main/java/com/entrecodigosycafe/flag/PanelForm.java
+++ flagPeru/src/main/java/com/entrecodigosycafe/flag/PanelForm.java
@@ -1,14 +1,41 @@
 package com.entrecodigosycafe.flag;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelForm extends JFrame {
     public PanelForm () {
         this.setTitle("Flag Peru");
-        this.setSize(700,500);
+        this.setSize(700,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        this.setResizable(false);
 
+        this.add(containerPrincipal());
         this.setVisible(true);
+    }
+
+    private JPanel containerPrincipal () {
+
+        JPanel pnlPrincipal = new JPanel();
+        pnlPrincipal.setLayout(new BoxLayout(pnlPrincipal, BoxLayout.X_AXIS));
+
+        JPanel pnlRedOne = new JPanel();
+        pnlRedOne.setBackground(Color.red);
+        pnlRedOne.setPreferredSize(new Dimension(250,200));
+
+        JPanel pnlWhiteTwo = new JPanel();
+        pnlWhiteTwo.setBackground(Color.white);
+        pnlWhiteTwo.setPreferredSize(new Dimension(250, 200));
+
+        JPanel pnlRedThree = new JPanel();
+        pnlRedThree.setBackground(Color.red);
+        pnlRedThree.setPreferredSize(new Dimension(250,200));
+
+        pnlPrincipal.add(pnlRedOne);
+        pnlPrincipal.add(pnlWhiteTwo);
+        pnlPrincipal.add(pnlRedThree);
+
+        return pnlPrincipal;
     }
 }
