f61b9cc76ecb2fa49477c324937e24ed09739ca3
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-10 20:36:10 -0500

    Hemos agregado paneles horizontales con los colores de la bandera alemana

--- flagGermany/src/main/java/com/entrecodigosycafe/germany/PanelForm.java
+++ flagGermany/src/main/java/com/entrecodigosycafe/germany/PanelForm.java
@@ -1,6 +1,7 @@
 package com.entrecodigosycafe.germany;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelForm extends JFrame {
 
@@ -11,6 +12,29 @@ public class PanelForm extends JFrame {
         this.setLocationRelativeTo(null);
         this.setResizable(false);
 
+        this.add(containerPrincipal());
         this.setVisible(true);
+    }
+
+    private JPanel containerPrincipal () {
+        JPanel pnlPrincipal = new JPanel();
+        pnlPrincipal.setLayout(new BoxLayout(pnlPrincipal, BoxLayout.Y_AXIS));
+
+        JPanel pnlBlack = new JPanel();
+        pnlBlack.setBackground(Color.black);
+        pnlBlack.setPreferredSize(new Dimension(700,200));
+
+        JPanel pnlRed = new JPanel();
+        pnlRed.setBackground(Color.red);
+        pnlRed.setPreferredSize(new Dimension(700, 200));
+
+        JPanel pnlYellow = new JPanel();
+        pnlYellow.setBackground(Color.yellow);
+        pnlYellow.setPreferredSize(new Dimension(700,200));
+
+        pnlPrincipal.add(pnlBlack);
+        pnlPrincipal.add(pnlRed);
+        pnlPrincipal.add(pnlYellow);
+        return pnlPrincipal;
     }
 }
