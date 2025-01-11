e815932ca01553a34a1500130c30f5bb9dff1714
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-10 19:27:12 -0500

    Hemos creado el JFrame de la aplicación

---
+++ flagGermany/src/main/java/com/entrecodigosycafe/germany/PanelForm.java
@@ -0,0 +1,16 @@
+package com.entrecodigosycafe.germany;
+
+import javax.swing.*;
+
+public class PanelForm extends JFrame {
+
+    public PanelForm () {
+        this.setTitle("Flag Germany");
+        this.setSize(700,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+
+        this.setVisible(true);
+    }
+}
