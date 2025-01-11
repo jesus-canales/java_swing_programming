36a4e001533356a3a161c99b3eae6e5d9396f699
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-11 00:01:10 -0500

    Se ha creado la ventana con JFrame

---
+++ flagSuiza/src/main/java/com/entrecodigosycafe/suiza/AppLauncher.java
@@ -0,0 +1,7 @@
+package com.entrecodigosycafe.suiza;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        new PanelForm();
+    }
+}
---
+++ flagSuiza/src/main/java/com/entrecodigosycafe/suiza/PanelForm.java
@@ -0,0 +1,16 @@
+package com.entrecodigosycafe.suiza;
+
+import javax.swing.*;
+
+public class PanelForm extends JFrame {
+
+    public PanelForm () {
+        this.setTitle("Bandera Suiza");
+        this.setSize(700, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+
+        this.setVisible(true);
+    }
+}
