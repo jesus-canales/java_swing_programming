7e71648ea457c164d6b522f94ab67d65706801a6
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-11 09:26:44 -0500

    Ya tenemos la versión final de la bandera Suiza

--- flagSuiza/src/main/java/com/entrecodigosycafe/suiza/PanelForm.java
+++ flagSuiza/src/main/java/com/entrecodigosycafe/suiza/PanelForm.java
@@ -21,16 +21,37 @@ public class PanelForm extends JFrame {
         pnlPrincipal.setBackground(Color.red);
         pnlPrincipal.setLayout(new GridBagLayout());
 
+        GridBagConstraints gbcv = new GridBagConstraints();
+
         JPanel verticalBar = new JPanel();
         verticalBar.setBackground(Color.white);
-        verticalBar.setPreferredSize(new Dimension(100,300));
+        verticalBar.setPreferredSize(new Dimension(150,400));
+        gbcv.gridx = 1;
+        gbcv.gridy = 0;
+        gbcv.gridwidth = 1;
+        gbcv.gridheight = 3;
+        gbcv.weightx = 1;
+        gbcv.weighty = 1;
+        gbcv.anchor = GridBagConstraints.CENTER;
+        gbcv.fill = GridBagConstraints.NONE;
+
+        GridBagConstraints gbch = new GridBagConstraints();
 
         JPanel horizontalBar = new JPanel();
         horizontalBar.setBackground(Color.white);
-        horizontalBar.setPreferredSize(new Dimension(300, 100));
+        horizontalBar.setPreferredSize(new Dimension(400, 150));
+        gbch.gridx = 0;
+        gbch.gridy = 1;
+        gbch.gridwidth = 3;
+        gbch.gridheight = 1;
+        gbch.weightx = 1;
+        gbch.weighty = 1;
+        gbch.anchor = GridBagConstraints.CENTER;
+        gbch.fill = GridBagConstraints.NONE;
 
-        pnlPrincipal.add(verticalBar, new GridBagConstraints());
-        pnlPrincipal.add(horizontalBar, new GridBagConstraints());
+
+        pnlPrincipal.add(verticalBar, gbcv);
+        pnlPrincipal.add(horizontalBar, gbch);
         return pnlPrincipal;
     }
 }
