8b0f4de9db691409d42b781340efbe97a02be988
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-11 01:01:21 -0500

    Tenemos el primer avance de la bandera suiza

--- flagSuiza/src/main/java/com/entrecodigosycafe/suiza/PanelForm.java
+++ flagSuiza/src/main/java/com/entrecodigosycafe/suiza/PanelForm.java
@@ -1,6 +1,7 @@
 package com.entrecodigosycafe.suiza;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelForm extends JFrame {
 
@@ -11,6 +12,25 @@ public class PanelForm extends JFrame {
         this.setLocationRelativeTo(null);
         this.setResizable(false);
 
+        this.add(containerPrincipal());
         this.setVisible(true);
+    }
+
+    private JPanel containerPrincipal () {
+        JPanel pnlPrincipal = new JPanel();
+        pnlPrincipal.setBackground(Color.red);
+        pnlPrincipal.setLayout(new GridBagLayout());
+
+        JPanel verticalBar = new JPanel();
+        verticalBar.setBackground(Color.white);
+        verticalBar.setPreferredSize(new Dimension(100,300));
+
+        JPanel horizontalBar = new JPanel();
+        horizontalBar.setBackground(Color.white);
+        horizontalBar.setPreferredSize(new Dimension(300, 100));
+
+        pnlPrincipal.add(verticalBar, new GridBagConstraints());
+        pnlPrincipal.add(horizontalBar, new GridBagConstraints());
+        return pnlPrincipal;
     }
 }
