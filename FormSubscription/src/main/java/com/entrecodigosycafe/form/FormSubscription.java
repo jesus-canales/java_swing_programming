d2775ada75206564a7a885d51093fdf598313e47
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-17 09:13:11 -0500

    Hemos agregado JLabel, JText y JButton que son los componentes que vamos a utilizar en el form

--- FormSubscription/src/main/java/com/entrecodigosycafe/form/FormSubscription.java
+++ FormSubscription/src/main/java/com/entrecodigosycafe/form/FormSubscription.java
@@ -6,7 +6,48 @@ import java.awt.*;
 public class FormSubscription extends JPanel {
 
     public FormSubscription() {
+        // Personalizamos el JPanel
         setBackground(new Color(217, 234, 253));
         setLayout(new GridBagLayout());
+
+        // Establecemos tamaño de elementos
+        Dimension sizeElements = new Dimension(150, 25);
+
+        JLabel lblName = new JLabel("Nombres: ");
+        lblName.setPreferredSize(sizeElements);
+
+        JTextField txtName = new JTextField();
+        txtName.setPreferredSize(sizeElements);
+
+        JLabel lblLastName = new JLabel("Apellidos: ");
+        lblLastName.setPreferredSize(sizeElements);
+
+        JTextField txtLastName = new JTextField();
+        txtLastName.setPreferredSize(sizeElements);
+
+        JLabel lblAge = new JLabel("Edad: ");
+        lblAge.setPreferredSize(sizeElements);
+
+        JTextField txtAge = new JTextField();
+        txtAge.setPreferredSize(sizeElements);
+
+        JLabel lblEmail = new JLabel("Correo: ");
+        lblEmail.setPreferredSize(sizeElements);
+
+        JTextField txtEmail = new JTextField();
+        txtEmail.setPreferredSize(sizeElements);
+
+        JButton btnSubmit = new JButton("Enviar");
+        btnSubmit.setPreferredSize(new Dimension(150, 40));
+
+        add(lblName);
+        add(txtName);
+        add(lblLastName);
+        add(txtLastName);
+        add(lblAge);
+        add(txtAge);
+        add(lblEmail);
+        add(txtEmail);
+        add(btnSubmit);
     }
 }
