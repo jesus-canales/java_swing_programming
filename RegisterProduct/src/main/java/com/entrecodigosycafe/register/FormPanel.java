c14e6c9710a7771863f4783934f9859ef7702b6a
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-17 14:07:01 -0500

    Hemos organizado los elementos de etiquetas, cajas de texto y lista de opciones

--- RegisterProduct/src/main/java/com/entrecodigosycafe/register/FormPanel.java
+++ RegisterProduct/src/main/java/com/entrecodigosycafe/register/FormPanel.java
@@ -26,7 +26,7 @@ public class FormPanel extends JPanel {
         lblPrice.setPreferredSize(sizeElements);
 
         txtPrice = new JTextField();
-        txtProduct.setPreferredSize(sizeElements);
+        txtPrice.setPreferredSize(sizeElements);
 
         JLabel lblCategory = new JLabel("Categoria: ");
         lblCategory.setPreferredSize(sizeElements);
@@ -44,13 +44,47 @@ public class FormPanel extends JPanel {
         txtQuantity = new JTextField();
         txtQuantity.setPreferredSize(sizeElements);
 
-        add(lblProduct);
-        add(txtProduct);
-        add(lblPrice);
-        add(txtPrice);
-        add(lblCategory);
-        add(cmbCategory);
-        add(lblQuantity);
-        add(txtQuantity);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5,5,5,5);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        add(lblProduct, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        add(txtProduct, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        add(lblPrice, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        add(txtPrice, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        add(lblCategory, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        add(cmbCategory, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        add(lblQuantity, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        add(txtQuantity, gbc);
     }
 }
