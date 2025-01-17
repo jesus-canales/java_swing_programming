c20b401e22d055bd8c3c9cac7504e99765aec847
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-17 11:31:17 -0500

    Hemos creado el proyecto y la ventana para el formulario

---
+++ RegisterProduct/src/main/java/com/entrecodigosycafe/register/AppLauncher.java
@@ -0,0 +1,8 @@
+package com.entrecodigosycafe.register;
+
+public class AppLauncher {
+
+    public static void main(String[] args) {
+        new ProductForm();
+    }
+}
---
+++ RegisterProduct/src/main/java/com/entrecodigosycafe/register/ProductForm.java
@@ -0,0 +1,16 @@
+package com.entrecodigosycafe.register;
+
+import javax.swing.*;
+
+public class ProductForm extends JFrame {
+
+    public ProductForm () {
+        setTitle("Formulario de Producto");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        setVisible(true);
+    }
+}
