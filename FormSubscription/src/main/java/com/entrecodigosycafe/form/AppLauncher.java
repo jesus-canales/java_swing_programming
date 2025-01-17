a2082a45b9a886ae5a4aa918dccc2e27d8546e45
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-17 08:36:41 -0500

    Hemos creado el proyecto, la clase de ventana y la clase principal.

---
+++ FormSubscription/src/main/java/com/entrecodigosycafe/form/AppLauncher.java
@@ -0,0 +1,9 @@
+package com.entrecodigosycafe.form;
+
+public class AppLauncher {
+
+    public static void main(String[] args) {
+
+        new PanelForm();
+    }
+}
---
+++ FormSubscription/src/main/java/com/entrecodigosycafe/form/PanelForm.java
@@ -0,0 +1,17 @@
+package com.entrecodigosycafe.form;
+
+import javax.swing.*;
+
+public class PanelForm extends JFrame {
+
+    public PanelForm() {
+        setTitle("Formulario de Suscripción");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 500);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        setVisible(true);
+    }
+
+}
