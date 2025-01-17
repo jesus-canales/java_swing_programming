a91c659f2bacaf5639fa980a48d62e7bf3465224
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-17 17:46:17 -0500

    Hemos creado el proyecto y agregado el soporte principal (JFrame)

---
+++ InputDataValidation/src/main/java/com/entrecodigosycafe/validations/AppLauncher.java
@@ -0,0 +1,8 @@
+package com.entrecodigosycafe.validations;
+
+public class AppLauncher {
+
+    public static void main(String[] args) {
+        new ValidationForm();
+    }
+}
---
+++ InputDataValidation/src/main/java/com/entrecodigosycafe/validations/ValidationForm.java
@@ -0,0 +1,17 @@
+package com.entrecodigosycafe.validations;
+
+import javax.swing.*;
+
+public class ValidationForm extends JFrame {
+
+    public ValidationForm() {
+        setTitle("Formulario de Validación de Datos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        setVisible(true);
+    }
+
+}
