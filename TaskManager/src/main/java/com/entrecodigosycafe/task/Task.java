db607cb3044d1128a65e9f7ff9b2b2509838c72d
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-18 13:29:55 -0500

    Hemos agregado la clase Task (tarea)

---
+++ TaskManager/src/main/java/com/entrecodigosycafe/task/Task.java
@@ -0,0 +1,32 @@
+package com.entrecodigosycafe.task;
+
+public class Task {
+
+    private String name;
+    private boolean isCompleted;
+
+    public Task(String name) {
+        this.name = name;
+        this.isCompleted = false;
+    }
+
+    public String getName() {
+        return name;
+    };
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String toString () {
+        return name + (isCompleted ? " completed" : "");
+    }
+}
