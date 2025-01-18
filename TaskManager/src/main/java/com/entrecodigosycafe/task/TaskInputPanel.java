bd0b0496f0f88a8b38e7b14a3a30e8f6eb006896
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-18 10:40:40 -0500

    Hemos organizado los componentes dentro del input de tareas

--- TaskManager/src/main/java/com/entrecodigosycafe/task/TaskInputPanel.java
+++ TaskManager/src/main/java/com/entrecodigosycafe/task/TaskInputPanel.java
@@ -14,12 +14,26 @@ public class TaskInputPanel extends JPanel {
         setLayout(new GridBagLayout());
 
         JLabel lblTaskName = new JLabel("Nombre de Tarea: ");
-        txtTaskName = new JTextField(40);
-        btnAddTask = new JButton("Agregar tarea");
+        txtTaskName = new JTextField(25);
+        txtTaskName.setPreferredSize(new Dimension(250, 25));
 
-        add(lblTaskName);
-        add(txtTaskName);
-        add(btnAddTask);
+        btnAddTask = new JButton("Agregar...");
+        btnAddTask.setPreferredSize(new Dimension(120, 30));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 5, 10, 5);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        add(lblTaskName, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        add(txtTaskName, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        add(btnAddTask, gbc);
 
     }
 }
