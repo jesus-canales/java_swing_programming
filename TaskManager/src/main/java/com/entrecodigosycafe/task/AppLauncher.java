b62158c3eff211031417b0bab769e64453b8d310
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-18 09:55:56 -0500

    Hemos creado el proyecto y el componente JFrame que es el soporte principal de la aplicación

---
+++ TaskManager/src/main/java/com/entrecodigosycafe/task/AppLauncher.java
@@ -0,0 +1,8 @@
+package com.entrecodigosycafe.task;
+
+public class AppLauncher {
+
+    public static void main(String[] args) {
+        new TaskManagerApp();
+    }
+}
---
+++ TaskManager/src/main/java/com/entrecodigosycafe/task/TaskManagerApp.java
@@ -0,0 +1,16 @@
+package com.entrecodigosycafe.task;
+
+import javax.swing.*;
+
+public class TaskManagerApp extends JFrame {
+
+    public TaskManagerApp () {
+        setTitle("Gestor de Tareas");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        setVisible(true);
+    }
+}
