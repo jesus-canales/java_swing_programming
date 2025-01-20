937b850c09a7bae6d74ae67345b1b312fa3a0a05
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-20 16:38:31 -0500

    Hemos creado el proyecto, agregado el componenente jdatapicker, también se ha creado la ventana de la aplicación

---
+++ ReservationRestaurant/src/main/java/com/entrecodigosycafe/reservation/AppLauncher.java
@@ -0,0 +1,8 @@
+package com.entrecodigosycafe.reservation;
+
+public class AppLauncher {
+
+    public static void main(String[] args) {
+        new ReservationApp();
+    }
+}
---
+++ ReservationRestaurant/src/main/java/com/entrecodigosycafe/reservation/ReservationApp.java
@@ -0,0 +1,15 @@
+package com.entrecodigosycafe.reservation;
+
+import javax.swing.*;
+
+public class ReservationApp extends JFrame {
+
+    public ReservationApp() {
+        setTitle("Aplicación de Reservas");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+    }
+}
