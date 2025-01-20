1e74e7b916eee81b9363aa2f64464c4ccefc76b6
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-01-20 18:13:08 -0500

    Hemos actualizado el pom y agregado la dependencia del time picker

---
+++ ReservationRestaurant/src/main/java/com/entrecodigosycafe/reservation/ReservationFormPanel.java
@@ -0,0 +1,17 @@
+package com.entrecodigosycafe.reservation;
+
+import com.github.lgooddatepicker.components.TimePicker;
+import org.jdatepicker.JDatePicker;
+
+import javax.swing.*;
+
+public class ReservationFormPanel extends JPanel {
+
+    private JTextField txtName;
+    private JTextField txtPhone;
+    private JDatePicker datePicker;
+    private TimePicker timePicker;
+    private JButton btnSubmit;
+
+
+}
