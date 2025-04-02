6c23f1585dcec152c7033a1d67db341f2c5d17c5
Jesús Canales <pjecanal@cibertec.edu.pe>
2025-04-02 07:29:22 -0500

    Actualizando tamaño de franjas en bandera Perú

--- flagPeru/src/main/java/com/entrecodigosycafe/flag/PanelForm.java
+++ flagPeru/src/main/java/com/entrecodigosycafe/flag/PanelForm.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class PanelForm extends JFrame {
     public PanelForm () {
         this.setTitle("Flag Peru");
-        this.setSize(700,600);
+        this.setSize(750,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
@@ -22,15 +22,15 @@ public class PanelForm extends JFrame {
 
         JPanel pnlRedOne = new JPanel();
         pnlRedOne.setBackground(Color.red);
-        pnlRedOne.setPreferredSize(new Dimension(250,200));
+        pnlRedOne.setPreferredSize(new Dimension(250,600));
 
         JPanel pnlWhiteTwo = new JPanel();
         pnlWhiteTwo.setBackground(Color.white);
-        pnlWhiteTwo.setPreferredSize(new Dimension(250, 200));
+        pnlWhiteTwo.setPreferredSize(new Dimension(250, 600));
 
         JPanel pnlRedThree = new JPanel();
         pnlRedThree.setBackground(Color.red);
-        pnlRedThree.setPreferredSize(new Dimension(250,200));
+        pnlRedThree.setPreferredSize(new Dimension(250,600));
 
         pnlPrincipal.add(pnlRedOne);
         pnlPrincipal.add(pnlWhiteTwo);
